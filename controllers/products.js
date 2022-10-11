adfe16388ae9ea2e96283d64582ce0378ed9e58b
jcolejeff <ikwuhj@gmail.com>
2022-10-11 13:51:51 +0100

    set up all products route

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,22 @@
+const Product = require("../models/product");
 const getAllProdctsStatic = async (req, res) => {
-	res.status(200).json({ msg: "products testing route" });
+	const Products = await Product.find({});
+
+	res.status(200).json({ Products });
 };
 const getAllProdcts = async (req, res) => {
-	res.status(200).json({ msg: "products route" });
+	const { featured, company, name } = req.query;
+	const queryObject = {};
+	if (featured) {
+		queryObject.featured = featured === "true" ? true : false;
+	}
+	if (company) {
+		queryObject.company = company;
+	}
+	if (name) {
+		queryObject.name = { $regex: name, $options: "i" };
+	}
+	const products = await Product.find(queryObject);
+	res.status(200).json({ products, nbHits: products.length });
 };
 module.exports = { getAllProdcts, getAllProdctsStatic };
